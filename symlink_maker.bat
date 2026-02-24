f43b77875d4e15c3a199d50abd31c164b0e83770
Xenthio <28588188+Xenthio@users.noreply.github.com>
2026-02-24 19:03:45 +1100

    Backup shader DLL and create symlink
    Check for an existing %gmodpath%\bin\win64\stdshader_dx6.dll and move it to stdshader_dx6.dll.bak if present, then create a symlink from the built stdshader_dx6.dll into the game's win64 folder. Adds echo messages for backup/link steps; preserves the existing addon and Lua binary symlink creation.

--- symlink_maker.bat
+++ symlink_maker.bat
@@ -4,6 +4,14 @@ echo Make sure you've built the debug build atleast once before running this
 set /p gmodpath="Game Path: "
 mklink /j "%gmodpath%\garrysmod\addons\remixbinary" ".\garrysmod\garrysmod\addons\remixbinary"
 echo Addon Linked.
+
+if exist "%gmodpath%\bin\win64\stdshader_dx6.dll" (
+    echo Backing up original stdshader_dx6.dll...
+    move /y "%gmodpath%\bin\win64\stdshader_dx6.dll" "%gmodpath%\bin\win64\stdshader_dx6.dll.bak"
+)
+mklink "%gmodpath%\bin\win64\stdshader_dx6.dll" "%cd%\garrysmod\bin\win64\stdshader_dx6.dll"
+echo Shader linked.
+
 mklink %gmodpath%\garrysmod\lua\bin\gmcl_RTXFixesBinary_win64.dll %cd%\x86_64\Debug\gmcl_RTXFixesBinary_win64.dll
 mklink %gmodpath%\garrysmod\lua\bin\gmcl_RTXFixesBinary_win64.exp %cd%\x86_64\Debug\gmcl_RTXFixesBinary_win64.exp
 mklink %gmodpath%\garrysmod\lua\bin\gmcl_RTXFixesBinary_win64.pdb %cd%\x86_64\Debug\gmcl_RTXFixesBinary_win64.pdb
