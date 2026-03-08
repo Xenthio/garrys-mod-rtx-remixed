ee140b522a482e0be2a96f68dc86c49c402569c9
CR <sambow23@gmail.com>
2026-03-08 01:15:11 -0600

    skybox_fix: blacklist vanilla gmod maps

--- garrysmod/garrysmod/addons/remixbinary/lua/remixlua/sv/sv_skybox_fix.lua
+++ garrysmod/garrysmod/addons/remixbinary/lua/remixlua/sv/sv_skybox_fix.lua
@@ -2,7 +2,14 @@ if not SERVER then return end
 
 -- Overrides "painted" shader-based skybox to prevent Remix from rasterizing
 
+local BLACKLISTED_MAPS = {
+	["gm_construct"] = true,
+	["gm_construct_rtx"] = true,
+	["gm_flatgrass"] = true,
+}
+
 hook.Add("PlayerInitialSpawn", "RTX_SkyboxFix", function(ply)
+	if BLACKLISTED_MAPS[game.GetMap()] then return end
 	if GetConVar("sv_skyname"):GetString() == "painted" then
 		RunConsoleCommand("sv_skyname", "sky_day01_01")
 	end
